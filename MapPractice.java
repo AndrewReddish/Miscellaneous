c02901ddd85af6894ef8d617516ff5de3e0b0420
Andrew Reddish <8884515+AndrewReddish@users.noreply.github.com>
2019-10-21 12:48:18 +0300

    Updated methods createArray, fillArray

--- MapPractice.java
+++ MapPractice.java
@@ -3,83 +3,44 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class MapPractice {
-    public static void main(String args[]) throws IOException {
-        System.out.println("Please enter array elements:");
-        int[] arr = createArray(2);
-        fillArray(arr);
-        System.out.println("the average of " + Arrays.toString(arr) + " equals " + findAverage(arr));
-    }
 
-    private static int[] createArray(int len) {
-        return new int[len];
-    }
+        public static void main(String args[]) throws IOException {
 
-    private static void fillArray(int [] array) throws IOException {
-
-        Scanner scan = new Scanner(System.in);
-        int value = 0;
-        for (int i = array[0]; i < array.length; i++){
-            for (int len = array[0]; len < array.length; value++){
-                array [value] = scan.nextInt();
-            }
+            int[] arr = createArray(2);
+            System.out.println("Enter array elements:");
+            fillArray(arr);
+            System.out.println("The average of " + Arrays.toString(arr) + " equals " + findAverage(arr));
         }
-    }
-    private static double findAverage(int[] list) {
-        double sum = 0;
-        if (list.length != 0){
-            for (int value : list) {
-                sum += value;
-            }
-            return sum/list.length;
-        } else if (list.length == 1){
-            return list [0];
-        }
-        else return 0;
 
-    }
+        private static int[] createArray(int len) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Define the length of array:");
+            len = scan.nextInt();
+            return new int[len];
+        }
+
+        private static void fillArray(int [] array) throws IOException {
+
+            Scanner scan = new Scanner(System.in);
+            //for (int i = array[0]; i <= array.length-1; i++){
+            int arrayIndex = 0;
+                while (arrayIndex < array.length){
+                    for (arrayIndex = 0; arrayIndex < array.length; arrayIndex++){
+                        array [arrayIndex] = scan.nextInt();
+                        System.out.println(Arrays.toString(array));
+                    }
+                }
+        }
+        private static double findAverage(int[] list) {
+            double sum = 0;
+            if (list.length != 0){
+                for (int value : list) {
+                    sum += value;
+                }
+                return sum/list.length;
+            } else if (list.length == 1){
+                return list [0];
+            }
+            else return 0;
+        }
 }
-
-/*    private static Map<String, String> ModifyMap(Map <String, String> map1) {
-        map1.remove("c");
-        if (map1.containsKey("a")){
-            map1.replace("b", map1.get("a"));
-
-            if (!map1.containsKey("b")) {
-                map1.put("b", map1.get("a"));
-            }
-            System.out.println(map1.values());
-        } else {
-            System.out.println("hello");
-        }
-        return map1;
-    }
-    private static Map<String, String> ModifyMap2(Map <String, String> map1) {
-        //String a = map1.get("a");
-        if (map1.containsKey("a") && map1.containsKey("b")){
-            map1.put("ab", map1.get("a") + map1.get("b"));
-            System.out.println(map1.values());
-        } else {
-            System.out.println("hello");
-        }
-        return map1;
-    }*/
-/*    private static Map<String, String> CreateMap() {
-        Map <String, String> countries = new HashMap<String, String>();
-        countries.put("US", "United States");
-        countries.put("GB", "Great Britain");
-        countries.put("UA", "Ukraine");
-        countries.put("ES", "Spain");
-
-        System.out.println(countries.get("US"));
-        return countries;
-    }
-
-    private static void ReturnValue(Map<String, String> countries) {
-        String key = "BB";
-        if (countries.containsKey(key)){
-            System.out.println(countries.get("UA")); ;
-        } else {
-            System.out.println(countries.values());
-        }
-
-    }*/
